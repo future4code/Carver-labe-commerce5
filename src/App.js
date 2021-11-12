2957be75f821f71a029bd1174ed9258e8ba983b9
gutto-moura <gmourah1792@gmail.com>
2021-11-12 10:14:01 -0300

    Arquivo filtros

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React from "react";
 
-import Produtos from './components/Produtos/Produtos';
-import Carrinho from './components/Carrinho/Carrinho';
-import { Filtro } from './components/Filtro/Filtro';
+import Produtos from "./components/Produtos/Produtos";
+import Carrinho from "./components/Carrinho/Carrinho";
+import { Filtro } from "./components/Filtro/Filtro";
+import Cards from "./components/Produtos/CardProduto"
 
 export default class App extends React.Component {
   state = {
-   minValue: "",
+    minValue: "",
     maxValue: "",
     searchName: "",
     listaCarrinho: [],
     listaProdutos: [
-      { id: 1, name: "Foguete da Missão Apollo 11", value: 1, imageUrl: "https://picsum.photos/200/200?random=1", },
-      { id: 2, name: "Satélite Antigo", value: 7, imageUrl: "https://picsum.photos/200/200?random=2", },
-      { id: 3, name: "Roupa Espacial", value: 99, imageUrl: "https://picsum.photos/200/200?random=3", },
-      { id: 4, name: "Meteorito", value: 11, imageUrl: "https://picsum.photos/200/200?random=4", },
-      { id: 5, name: "Aerolito", value: 10, imageUrl: "https://picsum.photos/200/200?random=5", },
-      { id: 6, name: "Viagem em Ônibus Espacial", value: 9, imageUrl: "https://picsum.photos/200/200?random=6", },
-    ]
-  }
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 1,
+        imageUrl: "https://picsum.photos/200/200?random=1",
+      },
+      {
+        id: 2,
+        name: "Satélite Antigo",
+        value: 7,
+        imageUrl: "https://picsum.photos/200/200?random=2",
+      },
+      {
+        id: 3,
+        name: "Roupa Espacial",
+        value: 99,
+        imageUrl: "https://picsum.photos/200/200?random=3",
+      },
+      {
+        id: 4,
+        name: "Meteorito",
+        value: 11,
+        imageUrl: "https://picsum.photos/200/200?random=4",
+      },
+      {
+        id: 5,
+        name: "Aerolito",
+        value: 10,
+        imageUrl: "https://picsum.photos/200/200?random=5",
+      },
+      {
+        id: 6,
+        name: "Viagem em Ônibus Espacial",
+        value: 9,
+        imageUrl: "https://picsum.photos/200/200?random=6",
+      },
+    ],
+  };
 
   // Funções
 
@@ -39,7 +70,7 @@ export default class App extends React.Component {
       return elemento.id !== idProduto;
     });
 
-    produtosExistentes.push(produtoAdicionado)
+    produtosExistentes.push(produtoAdicionado);
 
     if (produtoAdicionado.length < 1) {
       const novoProduto = {
@@ -47,46 +78,46 @@ export default class App extends React.Component {
         name: name,
         value: value,
         qtd: 1,
-      }
-      const novaLista = [...this.state.listaCarrinho, novoProduto]
+      };
+      const novaLista = [...this.state.listaCarrinho, novoProduto];
       this.setState({
         listaCarrinho: novaLista,
-      })
+      });
     } else {
       this.setState({
         listaCarrinho: produtosExistentes && carrinhoAtual,
-      })
+      });
     }
-  }
+  };
 
   // Remover carrinho:
   removerCarrinho = (idRemocao) => {
-    const carrinhoAtual = [...this.state.listaCarrinho]
+    const carrinhoAtual = [...this.state.listaCarrinho];
     let produtoDeletado;
 
     for (let i = 0; i < carrinhoAtual.length; i++) {
       if (carrinhoAtual[i].id === idRemocao) {
-        carrinhoAtual[i].qtd = carrinhoAtual[i].qtd -1;
+        carrinhoAtual[i].qtd = carrinhoAtual[i].qtd - 1;
         if (carrinhoAtual[i].qtd === 0) {
           produtoDeletado = true;
         }
       }
     }
 
-        if (produtoDeletado) {
+    if (produtoDeletado) {
       const deletarGeral = carrinhoAtual.filter((elemento) => {
         return elemento.id !== idRemocao;
-      })
-      
+      });
+
       this.setState({
         listaCarrinho: deletarGeral,
-      })
+      });
     } else {
       this.setState({
         listaCarrinho: carrinhoAtual,
-      })
+      });
     }
-  }
+  };
 
   // Filtros:
 
@@ -109,9 +140,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Produtos listaProdutos={this.state.listaProdutos} adicionarCarrinho={this.adicionarCarrinho} />
-        <Carrinho listaCarrinho={this.state.listaCarrinho} removerCarrinho={this.removerCarrinho} />
-        <Filtro>
+         <Filtro>
           minValue = {this.state.minValue}
           maxValue = {this.state.maxValue}
           searchName={this.state.searchName}
@@ -119,8 +148,38 @@ export default class App extends React.Component {
           updateMaxValue = {this.updateMaxValue}
           updateSearch = {this.updateSearch}
         </Filtro>
+        <Produtos
+          listaProdutos={this.state.listaProdutos}
+          adicionarCarrinho={this.adicionarCarrinho}
+        >
+          {this.state.listaProdutos
+            .filter((products) => {
+              return products.name
+                .toLowerCase()
+                .includes(this.state.searchName.toLowerCase());
+            })
+            .filter((products) => {
+              return (
+                this.state.minValue === "" ||
+                products.value >= this.state.minValue
+              );
+            })
+            .filter((products) => {
+              return (
+                this.state.maxValue === "" ||
+                products.value <= this.state.maxValue
+              );
+            })
+            .map(products => {
+                return <Cards key={products.id} products={products} />
+            })
+         }
+        </Produtos>
+        <Carrinho
+          listaCarrinho={this.state.listaCarrinho}
+          removerCarrinho={this.removerCarrinho}
+        />
       </div>
     );
-
   }
 }
